faa07d0821d29a25ded87be5558567e4b1656b0d
Marcelm <marcelclavero94@gmail.com>
2020-08-30 11:18:30 +0200

    Form en progreso, hook useMoneda, empezando useCriptomoneda

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from '@emotion/styled';
+
 import imagen from './img/cryptomonedas.png';
+import Formulario from './components/Formulario';
 
 const Contenedor = styled.div`
   max-width: 900px;
   margin: 0 auto;
-  @media (min-width: 992px) {
+  @media (min-width: 768px) {
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     column-gap: 2rem;
@@ -46,6 +48,7 @@ function App() {
       </div>
       <div>
         <Heading>Cotizar criptomonedas al instante</Heading>
+        <Formulario />
       </div>
     </Contenedor>
   );
